24662a68d4acc18087485c3109e9f5bc15e75201
Pedro Otero <pedronelson@cooxupe.com.br>
2024-04-17 15:15:04 -0300

    add mult-thread para pegar tag de varios leitores em paralelo mais ajustes de conversão de hex-to-string

---
+++ src/main/java/teste/socket/ConexaoRun.java
@@ -0,0 +1,85 @@
+package teste.socket;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
+import static teste.socket.SocketReaderRfid.converteHexUtf8;
+
+import static com.thingmagic.ReaderUtil.hexStringToByteArray;
+
+public class ConexaoRun implements Runnable{
+    private Socket socket;
+    private InputStream inputStream;
+    private PrintWriter printWriter;
+    private String response;
+
+   private String leitor;
+
+    public ConexaoRun(String leitor) {
+        this.leitor = leitor;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                System.out.println("Thread " + Thread.currentThread().getName() + " Leitor: " + leitor);
+                // Verifica se o socket está nulo ou não conectado
+                if (socket == null || !socket.isConnected()) {
+
+                    // Se estiver nulo ou não conectado, cria uma nova conexão
+                    socket = new Socket(leitor, 8081);
+
+                    inputStream = socket.getInputStream();
+
+                    printWriter = new PrintWriter(socket.getOutputStream());
+                    System.out.println("Conexão bem sucedida. ");
+
+                } else {
+                    System.out.println("Reutilizando conexão socket existente.");
+                }
+
+                // Envia um comando para a antena RFID
+                printWriter.println("read");
+                printWriter.flush();
+
+                // Lê a resposta da antena RFID
+                byte[] data = new byte[1024];
+                int bytesRead = inputStream.read(data);
+               // System.out.println("bytesRead: " + bytesRead);
+
+                // Converte a resposta para uma string
+                response = new String(data, 0, bytesRead, StandardCharsets.UTF_8);
+                //System.out.println("Resposta servidor: " + response + " Leitor: " + leitor);
+
+
+//                String hexString = "0x4358502E4255502D30393835";
+//                String utf8String = hexToUtf8(hexString);
+//                System.out.println("EPC String: " + utf8String);
+            //    if (response.contains("0x")) {
+                    String hexString = response;
+                   // System.out.println("response:  " + response);
+
+                    converteHexUtf8(hexString, leitor);
+
+
+              //  }
+
+                //return response;
+            } catch (IOException e) {
+                System.err.println("Erro na conexão socket: " + e.getMessage());
+                try {
+                    socket.close();
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+                socket = null; // Força a criação de uma nova conexão na próxima chamada
+            }
+        }
+
+    }
+    }
+
--- src/main/java/teste/socket/SocketReaderRfid.java
+++ src/main/java/teste/socket/SocketReaderRfid.java
@@ -1,6 +1,5 @@
 package teste.socket;
 
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +8,8 @@ import java.io.InputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 
 import static com.thingmagic.ReaderUtil.hexStringToByteArray;
 
@@ -18,80 +19,144 @@ public class SocketReaderRfid {
     private Socket socket;
     private InputStream inputStream;
     private PrintWriter printWriter;
+//    private String response;
+
+    public static void converteHexUtf8(String hexString, String leitor) {
+        try{
+            //System.out.println("Valor passado " + hexString);
+            if(hexString != null || hexString != "") {
+
+                // Encontrar o índice do próximo "0x" após o primeiro "0x"
+                int endIndex = hexString.indexOf("0x", 1 );
+                String[]  tag = hexString.split("0x");
+                //System.out.println("TAG: " + tag);
+
+                for(String tag2 : tag){
+                    if(!tag2.isEmpty() && !tag2.isBlank()) {
+                       // System.out.println("TAG2: " + tag2);
+                        byte[] bytes = hexStringToByteArray(tag2.replace("\r\n", ""));
+
+                        // Decodificar os bytes usando UTF-8
+                        String utf8String = new String(bytes, StandardCharsets.UTF_8);
+                        String tratada = utf8String.substring(0, utf8String.length());
+                        System.out.println("String UTF-8: " + tratada + "  Leitor: " + leitor);
+                    }
+                }
+//                System.out.println("endIndex primeiro if: " + endIndex);
+//                // Se não houver mais "0x", o valor continua até o final da string
+//                if (endIndex == -1) {
+//                    endIndex = hexString.length();
+//                    System.out.println("Substring extraída if:  " + hexString);
+//                } else {
+//                    endIndex = endIndex;
+//                    System.out.println("Substring extraída: " + hexString.substring(2, endIndex));
+
+                    // Converter a sequência hexadecimal em um array de bytes
+//                    byte[] bytes = hexStringToByteArray(hexString.substring(2, endIndex));
+//
+//                    // Decodificar os bytes usando UTF-8
+//                    String utf8String = new String(bytes, StandardCharsets.UTF_8);
+//                    String tratada = utf8String.substring(0, utf8String.length() - 1);
+//                    System.out.println("String UTF-8: " + tratada);
+
+                    //System.out.println("Resposta servidor sem conversão: " + data);
+
+
+            }
+    }catch (Exception e){
+            System.out.println("Exception Conversão: " + e + " valor " + hexString);
+        }
+    }
 
     @GetMapping("/readtag")
-    public void readTag() throws IOException {
-        String response;
+    public void readTag() throws IOException, InterruptedException {
+        List<String> urls = new ArrayList<>();
+        urls.add("172.16.254.215");
+        urls.add("172.16.254.216");
+        urls.add("172.16.243.49");
 
-        while (true) {
-            try {
-                // Verifica se o socket está nulo ou não conectado
-                if (socket == null || !socket.isConnected()) {
-                    // Se estiver nulo ou não conectado, cria uma nova conexão
-                    socket = new Socket("172.16.254.215", 8081);
-                    inputStream = socket.getInputStream();
-                    printWriter = new PrintWriter(socket.getOutputStream());
-
-                } else {
-                    System.out.println("Reutilizando conexão socket existente.");
-                }
-
-                // Envia um comando para a antena RFID
-                printWriter.println("read");
-                printWriter.flush();
-
-                // Lê a resposta da antena RFID
-                byte[] data = new byte[1024];
-                int bytesRead = inputStream.read(data);
-
-                // Converte a resposta para uma string
-                response = new String(data, 0, bytesRead, StandardCharsets.UTF_8);
-                System.out.println("Resposta servidor: " + response);
-
-//                String hexString = "0x4358502E4255502D30393835";
-//                String utf8String = hexToUtf8(hexString);
-//                System.out.println("EPC String: " + utf8String);
-                if (response.startsWith("0x")) {
-                    String hexString = response;
-
-                    converteHexUtf8(hexString);
-
-                }
+        String response = null;
 
 
+        Thread thread = new Thread(new ConexaoRun("172.16.254.215"));
+        thread.start();
 
+        Thread thread2 = new Thread(new ConexaoRun("172.16.254.216"));
+        thread2.start();
 
-                //return response;
-            } catch (IOException e) {
-                System.err.println("Erro na conexão socket: " + e.getMessage());
-                socket = null; // Força a criação de uma nova conexão na próxima chamada
-            }
+        Thread thread3 = new Thread(new ConexaoRun("172.16.243.49"));
+        thread3.start();
+
+    while (true) {
+        Thread.sleep(250);
+        if (!thread.isAlive()) {
+            thread = new Thread(new ConexaoRun("172.16.254.215"));
+            thread.start();
+            System.out.println("Reinstanciando a Thread " + Thread.currentThread().getName() + "IP:  172.16.254.215");
         }
 
-    }
+        if (!thread2.isAlive()) {
+            thread2 = new Thread(new ConexaoRun("172.16.254.216"));
+            thread2.start();
+            System.out.println("Reinstanciando a Thread " + Thread.currentThread().getName() + "IP: 172.16.254.216");
+        }
 
-    private static void converteHexUtf8(String hexString) {
-        // Encontrar o índice do próximo "0x" após o primeiro "0x"
-        int endIndex = hexString.indexOf("0x", 2);
-        // Se não houver mais "0x", o valor continua até o final da string
-        if (endIndex == -1) {
-            endIndex = hexString.length();
-        }else{
-            endIndex = endIndex;
-            System.out.println("Substring extraída: " + hexString.substring(2,endIndex));
-
-            // Converter a sequência hexadecimal em um array de bytes
-            byte[] bytes = hexStringToByteArray(hexString.substring(2,endIndex));
-
-            // Decodificar os bytes usando UTF-8
-            String utf8String = new String(bytes, StandardCharsets.UTF_8);
-            String tratada = utf8String.substring(0, utf8String.length()-1);
-            System.out.println("String UTF-8: " + tratada);
-
-
-            //System.out.println("Resposta servidor sem conversão: " + data);
-
+        if (!thread3.isAlive()) {
+            thread3 = new Thread(new ConexaoRun("172.16.243.49"));
+            thread3.start();
+            System.out.println("Reinstanciando a Thread " + Thread.currentThread().getName() + "IP: 172.16.243.49");
         }
     }
+//        while (true) {
+//            for (String uri : urls) {
+//                try {
+//                // Verifica se o socket está nulo ou não conectado
+//                if (socket == null || !socket.isConnected()) {
+//                    System.out.println("passou aqui 1");
+//                    // Se estiver nulo ou não conectado, cria uma nova conexão
+//                    socket = new Socket(uri, 8081);
+//                    System.out.println("passou aqui 2");
+//                    inputStream = socket.getInputStream();
+//                    System.out.println("passou aqui 3");
+//                    printWriter = new PrintWriter(socket.getOutputStream());
+//                    System.out.println("Conexão bem sucedida. " + uri);
+//
+//                } else {
+//                    System.out.println("Reutilizando conexão socket existente.");
+//                }
+//
+//                // Envia um comando para a antena RFID
+//                printWriter.println("read");
+//                printWriter.flush();
+//
+//                // Lê a resposta da antena RFID
+//                byte[] data = new byte[1024];
+//                int bytesRead = inputStream.read(data);
+//                System.out.println("bytesRead: " + bytesRead);
+//
+//                // Converte a resposta para uma string
+//                response = new String(data, 0, bytesRead, StandardCharsets.UTF_8);
+//                System.out.println("Resposta servidor: " + response + " Leitor: " + uri);
+//
+//                //String utf8String = hexToUtf8(response);
+//                //System.out.println("EPC String: " + utf8String);
+//                if (response.startsWith("0x")) {
+//                    //String hexString = response;
+//                    System.out.println("response:  " + response);
+//
+//                    converteHexUtf8(response);
+//
+//                }
+//
+//                //return response;
+//                } catch (IOException e) {
+//                System.err.println("Erro na conexão socket: " + e.getMessage());
+//                socket.close();
+//                socket = null; // Força a criação de uma nova conexão na próxima chamada
+//                }
+//
+//            }
 
+//        }
+    }
 }
